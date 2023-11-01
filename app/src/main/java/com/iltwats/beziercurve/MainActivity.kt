60fd9251aa0c5adf9f620337dd62674d1a871ddd
Iltwats <2012atulsharma@gmail.com>
2023-11-01 22:51:42 +0530

    Initial commit

---
+++ app/src/main/java/com/iltwats/beziercurve/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.iltwats.beziercurve
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.iltwats.beziercurve.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var activityMainBinding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMainBinding.root)
+
+        activityMainBinding.curve.setCurveBorderColor(R.color.blue)
+        activityMainBinding.curve.addDataPoints(generateDataPoint())
+
+    }
+
+    private fun generateDataPoint(): List<DataPoint> {
+        val list = mutableListOf<DataPoint>()
+        val range = (0..10)
+
+        (1..3).forEach { it ->
+            when(it){
+                1->list.add(DataPoint(1f))
+                2->list.add(DataPoint(80f))
+                3->list.add(DataPoint(1f))
+            }
+            //list.add(DataPoint(range.random()*100f))
+        }
+        return list
+    }
+}
+
